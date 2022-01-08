97aa2503836af6e11a668b5b98b3abd9e099d47e
Jagdish <learnitsolutions1983@gmail.com>
2022-01-08 17:31:35 +0530

    init

--- modules/ec2/main.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_instance" "myec2" {
-   ami = "ami-052cef05d01020f1d"
-   instance_type = "t2.micro"
-   
-}
--- provider.tf
+++ provider.tf
@@ -1,12 +1,16 @@
   terraform {
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 3.66"
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 2.80"
     }
   }
 }
 
-provider "aws" {
-  region = "ap-south-1"
+provider "azurerm" {
+  # Configuration options
+  features {}
+  subscription_id = "20cbbc2c-4a13-43a6-a37a-5ecfc1bb495b"
 }
+
+
